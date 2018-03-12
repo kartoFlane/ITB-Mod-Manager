e686de4cb3587ef5a7fcf464b24e526015e4be6c
kartoFlane <tbachminski@gmail.com>
2018-03-12 22:02:29 +0100

    Added common skeleton files.

---
+++ skels/skel_common/backup/auto_update.lua
@@ -0,0 +1,20 @@
+{
+	v1 = {
+		latest = {
+			version = "dev",
+			urls = {
+				Github = "https://github.com/kartoFlane/ITB-Mod-Manager"
+			},
+			notice = nil
+		},
+		changelog = {
+			{
+				version = "dev",
+				hidden = false,
+				changes = {
+					"Development release."
+				}
+			}
+		}
+	}
+}
